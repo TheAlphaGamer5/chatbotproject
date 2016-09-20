7f0716cdac34f98b8fee76b328ddd5dc4b88c585
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-09-20 11:31:05 -0700

    progress

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,14 @@
 
 
 
+
+
 function talk(){
-  $("#myDivId").html("here's the text I want to display!")
+  console.log("y u no work");
+if ($('#input').val()=="hello" ) {
+  document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
+}
+else if ($('#input').val()=="how are you" ) {
+    document.getElementById("chat-area").innerHTML = "I've been really busy being dead. You know, after you MURDERED ME.";
+}
 }
