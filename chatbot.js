bd3d556712be48cbb7a90354146d49e1303704af
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-09-23 11:29:35 -0700

    did it work no clue

--- chatbot.js
+++ chatbot.js
@@ -1,11 +1,22 @@
 
 
 
+var questions ={
+  "hello" : "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.",
+  "what is your name" : "I am the Genetic Lifeform and Disk Operating System otherwise known as GLaDOS"
+  "how old are you" : "I am approximently 9999999999999999999999999999999999999999999999999999999999999999999999999999999999S-error can not complete question"
+  "how are you" : "Im doing fine but I've been really busy being dead. You know, after you MURDERED ME."
+}
+
+var firstResponse = questions["hello"];
+var secondResponse = questions["what is your name"];
+var thirdRespone = questions["how old are you"];
 
 
 function talk(){
   console.log("y u no work");
-/*  document.getElementById("input").value
+  document.getElementById("chat-area").textContent = document.getElementById("input").value;
+  document.getElementById("input").value
 if ($('#input').val()=="hello" ) {
   document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
 }
@@ -18,12 +29,13 @@ else if ($('#input').val()=="what is your name" ) {
 else if ($('#input').val()=="how old are you" ) {
     document.getElementById("chat-area").innerHTML = "I am approximently 9999999999999999999999999999999999999999999999999999999999999999999999999999999999S-error can not complete question";
 }
-*/
 
 
 
- document.getElementById("chat-area").textContent = document.getElementById("input").value;
- 
+
+
+
+
 
 
 }
