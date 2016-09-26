228f7abd7686ad305f5abb616e9e6ad73850a8c6
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-09-26 12:00:26 -0700

    lots and lots of responses

--- chatbot.js
+++ chatbot.js
@@ -22,10 +22,43 @@ function talk(){
 if ($('#input').val()=="hello" ) {
   document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
 }
+else if ($('#input').val()=="hey" ) {
+  document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
+}
+else if ($('#input').val()=="sup" ) {
+  document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
+}
+else if ($('#input').val()=="whats up" ) {
+  document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
+}
+else if ($('#input').val()=="howdy" ) {
+  document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
+}
+else if ($('#input').val()=="hola" ) {
+  document.getElementById("chat-area").innerHTML = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
+}
 else if ($('#input').val()=="how are you" ) {
     document.getElementById("chat-area").innerHTML = "Im doing fine but I've been really busy being dead. You know, after you MURDERED ME.";
 }
+else if ($('#input').val()=="how you doing" ) {
+    document.getElementById("chat-area").innerHTML = "Im doing fine but I've been really busy being dead. You know, after you MURDERED ME.";
+}
+else if ($('#input').val()=="hows life" ) {
+    document.getElementById("chat-area").innerHTML = "Im doing fine but I've been really busy being dead. You know, after you MURDERED ME.";
+}
+else if ($('#input').val()=="how's life" ) {
+    document.getElementById("chat-area").innerHTML = "Im doing fine but I've been really busy being dead. You know, after you MURDERED ME.";
+}
+else if ($('#input').val()=="how is life" ) {
+    document.getElementById("chat-area").innerHTML = "Im doing fine but I've been really busy being dead. You know, after you MURDERED ME.";
+}
 else if ($('#input').val()=="what is your name" ) {
+    document.getElementById("chat-area").innerHTML = "I am the Genetic Lifeform and Disk Operating System otherwise known as GLaDOS";
+}
+else if ($('#input').val()=="what's your name" ) {
+    document.getElementById("chat-area").innerHTML = "I am the Genetic Lifeform and Disk Operating System otherwise known as GLaDOS";
+}
+else if ($('#input').val()=="whats your name" ) {
     document.getElementById("chat-area").innerHTML = "I am the Genetic Lifeform and Disk Operating System otherwise known as GLaDOS";
 }
 else if ($('#input').val()=="how old are you" ) {
