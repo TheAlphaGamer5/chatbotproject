11c0f6a78c665a837957fe7ab366947e3f11b8db
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-09-30 11:32:02 -0700

    dates and stuff

--- chatbot.js
+++ chatbot.js
@@ -7,6 +7,8 @@ inputTalk = document.getElementById("input").value;
 question = "User: " + inputTalk + "<br>";
 document.getElementById("chat-area").innerHTML += question;
 
+var weekdayArray = ["it is Sunday", " it is Monday", " it is Tuesday", " it is Wednesday", " it is Thursday", " it is Friday", " it is Saturday"];
+
 
 var hi = question.search(/hello/i)
 var how = question.search(/how are you/i);
@@ -16,9 +18,10 @@ var time = question.search(/time/i);
 var year = question.search(/year/i);
 var day = question.search(/day/i);
 var date = question.search(/date/i);
-var song = question.search(/sing a song/i);
+var song = question.search(/sing/i);
 var picture = question.search(/picture/i);
-//var da = new Date(Date.now());
+var da = new Date(Date.now());
+
 //  console.log("y u no work");
   //document.getElementById("chat-area").textContent = document.getElementById("input").value;
 
@@ -38,6 +41,18 @@ else if (old > -1) {
 }
 else if (song > -1) {
     document.getElementById("chat-area").innerHTML += "GLaDOS: This was a triumph.I'm making a note here:HUGE SUCCESS.It's hard to overstate my satisfaction.Aperture Science.We do what we must because we can.For the good of all of us Except the ones who are dead. But there's no sense crying over every mistake.You just keep on trying till you run out of cake.And the science gets done and you make a neat gun.For the people who are still alive.I'm not even angry.I'm being so sincere right now.Even though you broke my heart and killed me.And tore me to pieces.And threw every piece into a fire.As they burned it hurt because I was so happy for you! Now these points of data make a beautiful line.And we're out of beta, we're releasing on time.So I'm GLaD I got burned.Think of all the things we learned For the people who are still alive. Go ahead and leave me. I think I prefer to stay inside. Maybe you'll find someone else to help you. Maybe Black Mesa... THAT WAS A JOKE. Haha. FAT CHANCE. Anyway, this cake is great. It's so delicious and moist. Look at me still talking when there's science to do. When I look out there it makes me GLaD I'm not you. I've experiments to run there is research to be done  On the people who are still alive And believe me I am still alive. I'm doing science and I'm still alive. I feel FANTASTIC and I'm still alive. While you're dying I'll be still alive. And when you're dead I will be still alive. Still alive Still alive"  + "<br>";
+}
+else if (time > -1) {
+  document.getElementById("chat-area").innerHTML += "GLaDOS: the time is" +" " + da  + "<br>";
+}
+else if (year > -1) {
+  document.getElementById("chat-area").innerHTML += "GLaDOS: the year is" +" " + da.getFullYear()  + "<br>";
+}
+else if (day > -1) {
+  document.getElementById("chat-area").innerHTML += "GLaDOS: today is " +" " + da.getUTCDay();  + "<br>";
+}
+else if (date > -1) {
+  document.getElementById("chat-area").innerHTML += "GLaDOS: today is the" +" " + da.getDate() +"th"  + "<br>";
 }
 else {
     document.getElementById("chat-area").innerHTML += "GLaDOS: please, state something actually meaningful."  + "<br>";
